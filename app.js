2a3ff7c906f60b30dcd56a3a297361d50d4e91af
Luis Otavio <twu125@hotmail.com>
2024-05-22 04:49:29 -0300

    003

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', function () {
         menuContent.style.display = menuContent.style.display === 'block' ? 'none' : 'block';
     });
 
+    
+
     // Fecha o menu se clicar fora dele
     document.addEventListener('click', function (event) {
         if (!menuContent.contains(event.target) && menuContent.style.display === 'block') {
@@ -25,6 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
             this.classList.add('button-selected');
         });
     });
+    
 
     var buttons = document.querySelectorAll('.button');
 
@@ -73,39 +76,61 @@ document.addEventListener('DOMContentLoaded', function () {
         adicionarButton.addEventListener('click', function () {
         });
     }
-    window.onload = function () {
-        var alertaButton = document.getElementById('alertaButton');
-
-        var banhoAudio = new Audio('sound/banho.mp3');
-
-        var currentAudio = null;
-
-        function playAudio(audio) {
-            if (currentAudio) {
-                currentAudio.pause();
-            }
-            currentAudio = audio;
-            audio.play();
-        }
-
-        alertaButton.addEventListener('click', function () {
-            playAudio(banhoAudio);
-        });
-
-    }
-
-    window.onload = function() {
-        var samuButton = document.querySelector('.button i.fa-ambulance');
-    
-        samuButton.addEventListener('click', function() {
-            window.location.href = 'tel:192';
-        });
-    }
-
-
-
 
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuButton = document.querySelector('.menu-button');
+    const menuContent = document.querySelector('.menu-content');
+
+    menuButton.addEventListener('click', function (event) {
+        event.stopPropagation(); // Impede que o clique se propague para o documento
+        menuContent.style.display = menuContent.style.display === 'block' ? 'none' : 'block';
+    });
+
+    
+
+    // Fecha o menu se clicar fora dele
+    document.addEventListener('click', function (event) {
+        if (!menuContent.contains(event.target) && menuContent.style.display === 'block') {
+            menuContent.style.display = 'none';
+        }
+    });
+
+    document.querySelectorAll('.button-grid .button').forEach(button => {
+        button.addEventListener('click', function () {
+            // Remove a classe 'button-selected' de todos os botões
+            document.querySelectorAll('.button-selected').forEach(selectedButton => {
+                selectedButton.classList.remove('button-selected');
+            });
+
+            // Adiciona a classe 'button-selected' ao botão clicado
+            this.classList.add('button-selected');
+        });
+    });
+    
+
+    var buttons = document.querySelectorAll('.button');
+
+    // Adiciona um ouvinte de evento 'click' a cada botão
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            // Adiciona a classe 'button-selected'
+            this.classList.add('button-selected');
+        });
+    
+        
+
+    });
+
+    const samuButton = document.getElementById('samuButton');
+    samuButton.addEventListener('click', function() {
+        window.location.href = 'tel:192';
+    });
+});
+
+
+
 
 
 
